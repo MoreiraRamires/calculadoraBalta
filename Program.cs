c09d8f3c75427b0badeada20410bb85547355d78
Rafael Ramires <moreira.ramires@gmail.com>
2022-08-03 11:42:58 -0300

    calculando valores enviados pelo usuario

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.Clear();
+Console.WriteLine("Primeiro Valor");
+float valor1 = float.Parse(Console.ReadLine());
+Console.WriteLine("Segundo Valor");
+float valor2 = float.Parse(Console.ReadLine());
+Console.WriteLine(valor1+valor2);
