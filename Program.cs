1aeea6bf157fc96cf5fce04df4640142ea1aaf7f
Rafael Ramires <moreira.ramires@gmail.com>
2022-08-03 12:04:37 -0300

    testando funcoes

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.Clear();
-Console.WriteLine("Primeiro Valor");
-float valor1 = float.Parse(Console.ReadLine());
-Console.WriteLine("Segundo Valor");
-float valor2 = float.Parse(Console.ReadLine());
+Soma(5,10);
 
-float resultado = valor1 / valor2;
-Console.WriteLine($"O resultado é {resultado} " );
+static void Soma (float valor1 , float valor2){
+    Console.WriteLine("Primeiro Valor");
+    // float valor1 = float.Parse(Console.ReadLine());
+    Console.WriteLine("Segundo Valor");
+    // float valor2 = float.Parse(Console.ReadLine());
+
+    float resultado = valor1 + valor2;
+    Console.WriteLine($"O resultado é {resultado} " );
+}
