b43d062099dadd0c4b0d29634987f74273191ace
Rafael Ramires <moreira.ramires@gmail.com>
2022-08-03 14:02:05 -0300

    4 operacoes basicas => checked

--- Program.cs
+++ Program.cs
@@ -1,14 +1,55 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.Clear();
-Soma(5,10);
 
-static void Soma (float valor1 , float valor2){
+// Soma();
+// Subtracao();
+// Divisao();
+Multiplicacao();
+
+static void Soma (float FirstArg, float SecondArg){
+    Console.Clear();
+
     Console.WriteLine("Primeiro Valor");
-    // float valor1 = float.Parse(Console.ReadLine());
+    float valor1 =  float.Parse(Console.ReadLine()) + FirstArg;
     Console.WriteLine("Segundo Valor");
-    // float valor2 = float.Parse(Console.ReadLine());
+    float valor2 = float.Parse(Console.ReadLine());
 
     float resultado = valor1 + valor2;
-    Console.WriteLine($"O resultado é {resultado} " );
+    Console.WriteLine($"O resultado da soma é {resultado} " );
+}
+
+static void Subtracao(){
+    Console.Clear();
+
+    Console.WriteLine("Primeiro Valor");
+    float valor1 = float.Parse(Console.ReadLine());
+    Console.WriteLine("Segundo Valor");
+    float valor2 = float.Parse(Console.ReadLine());
+    float resultado =  valor1 - valor2;
+    Console.WriteLine($"O resultado da subtração é {resultado}");
+}
+
+static void Divisao (){
+    Console.Clear();
+    Console.WriteLine("Primeiro Valor: ");
+    float valor1 = float.Parse( Console.ReadLine());
+    Console.WriteLine("Segundo Valor: ");
+    float valor2 = float.Parse( Console.ReadLine());
+
+    float resultado =  valor1 / valor2;
+    Console.WriteLine($"O resultado da divisão é {resultado}");
+
+}
+
+static void Multiplicacao (){
+    Console.Clear();
+    Console.WriteLine("Primeiro Valor: ");
+    float valor1 = float.Parse( Console.ReadLine());
+    Console.WriteLine("Segundo Valor: ");
+    float valor2 = float.Parse( Console.ReadLine());
+
+    float resultado =  valor1 * valor2;
+    Console.WriteLine($"O resultado da multipicacao é {resultado}");
+
 }
